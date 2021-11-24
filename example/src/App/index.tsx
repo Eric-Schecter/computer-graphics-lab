55646a00d3595fcf3da87e562bdbed92c9a97a12
Eric-Schecter <1090165925@qq.com>
2021-11-24 11:23:18 +0800

    change camera pos and lookat

--- example/src/App/index.tsx
+++ example/src/App/index.tsx
@@ -7,7 +7,7 @@ const rawData = [
   { position: { x: 200, y: 0, z: 0 }, size: { width: 1, height: 200, depth: 100 }, color: { x: 0., y: 1, z: 0. }, emissive: { x: 0, y: 0, z: 0 } },
   { position: { x: 0, y: 0, z: -100 }, size: { width: 200, height: 200, depth: 1 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 0, y: 0, z: 0 } },
   { position: { x: 0, y: -100, z: 0 }, size: { width: 200, height: 1, depth: 100 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 0, y: 0, z: 0 } },
-  { position: { x: -50, y: 0, z: 50 }, size: { width: 30, height: 60, depth: 30 }, color: { x: 0.5, y: 0.3, z: 0.6 }, emissive: { x: 0, y: 0, z: 0 } },
+  { position: { x: -50, y: -10, z: 50 }, size: { width: 30, height: 60, depth: 30 }, color: { x: 0.5, y: 0.3, z: 0.6 }, emissive: { x: 0, y: 0, z: 0 } },
 ]
 
 export const App = () => {
