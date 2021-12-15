e87ca96b12ba91446b87d99cc789981f1d3fff40
Eric-Schecter <1090165925@qq.com>
2021-12-16 00:13:48 +0800

    update readme

--- README.md
+++ README.md
@@ -20,23 +20,44 @@ use as a React component
 import Page from '@youyouzone/react-sdf';
 
 export const App = () => {
-  return <Page style={{width:'100%',height:'100vh'}}>
-    <sphere
-      position={{ x: 0, y: 1, z: 0 }}
-      radius={1.0}
-      color={{ x: .5, y: .3, z: .6 }}
-      diffuse={1}
-      specular={0.01}
+  const rawData = [
+    { position: { x: 0, y: 200, z: 0 }, size: { width: 100, height: 1, depth: 50 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 1, y: 1, z: 1 }, roughness: 0, specular: 0 },
+    { position: { x: 0, y: 201, z: 0 }, size: { width: 200, height: 1, depth: 100 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 0 },
+    { position: { x: -200, y: 0, z: 0 }, size: { width: 1, height: 200, depth: 100 }, color: { x: 1, y: 0., z: 0. }, emissive: { x: 0, y: 0, z: 0 }, roughness: 1, specular: 1 },
+    { position: { x: 200, y: 0, z: 0 }, size: { width: 1, height: 200, depth: 100 }, color: { x: 0., y: 1, z: 0. }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0.1, specular: 1 },
+    { position: { x: 0, y: 0, z: -100 }, size: { width: 200, height: 200, depth: 1 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 1 },
+    { position: { x: 0, y: -100, z: 0 }, size: { width: 200, height: 1, depth: 100 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 0 },
+    { position: { x: -50, y: -20, z: 50 }, size: { width: 30, height: 60, depth: 30 }, color: { x: 0.5, y: 0.3, z: 0.6 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 0 },
+  ]
+
+  return <Page
+    style={{ width: '100%', height: '100vh' }}
+  >
+    <Camera
+      position={{ x: 0, y: 50, z: 250 }}
+      lookat={{ x: 0, y: 50, z: 0 }}
+      rotation={0}
+      fov={50 / 180 * Math.PI}
     />
+    {rawData.map(({ position, size, color, emissive, roughness, specular }, i) =>
+      <box
+        key={i}
+        position={position}
+        size={size}
+        color={color}
+        emissive={emissive}
+        roughness={roughness}
+        specular={specular}
+      />
+    )}
     <sphere
-      position={{ x: -1.5, y: 1, z: 0 }}
-      radius={0.1}
-      color={{ x: .5, y: .3, z: .6 }}
-    />
-    <box
-      position={{ x: 1.5, y: 1, z: 0 }}
-      size={{width:0.1,height:0.1,depth:0.1}}
-      color={{ x: .5, y: .3, z: .6 }}
+      ref={ref}
+      position={{ x: 50, y: -50, z: 0 }}
+      radius={50}
+      color={{ x: 0.5, y: 0.3, z: 0.6 }}
+      emissive={{ x: 0., y: 0., z: 0. }}
+      roughness={0}
+      specular={1}
     />
   </Page>
 }
--- example/src/App/index.tsx
+++ example/src/App/index.tsx
@@ -1,37 +1,16 @@
-import Page, { Camera, useFrame, SphereProp } from '@youyouzone/react-sdf';
-import { useEffect, useRef, useState } from 'react';
-
-const rawData = [
-  { position: { x: 0, y: 200, z: 0 }, size: { width: 100, height: 1, depth: 50 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 1, y: 1, z: 1 }, roughness: 0, specular: 0 },
-  { position: { x: 0, y: 201, z: 0 }, size: { width: 200, height: 1, depth: 100 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 0 },
-  { position: { x: -200, y: 0, z: 0 }, size: { width: 1, height: 200, depth: 100 }, color: { x: 1, y: 0., z: 0. }, emissive: { x: 0, y: 0, z: 0 }, roughness: 1, specular: 1 },
-  { position: { x: 200, y: 0, z: 0 }, size: { width: 1, height: 200, depth: 100 }, color: { x: 0., y: 1, z: 0. }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0.1, specular: 1 },
-  { position: { x: 0, y: 0, z: -100 }, size: { width: 200, height: 200, depth: 1 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 1 },
-  { position: { x: 0, y: -100, z: 0 }, size: { width: 200, height: 1, depth: 100 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 0 },
-  { position: { x: -50, y: -20, z: 50 }, size: { width: 30, height: 60, depth: 30 }, color: { x: 0.5, y: 0.3, z: 0.6 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 0 },
-]
-
-const Sphere = () => {
-  const ref = useRef<SphereProp>();
-  // useFrame((delta: number,time:number) => {
-  //   if (ref.current){
-  //     (ref.current.position as any).setData([50,Math.sin(time/1000) * 100,0],'position');
-  //     // ref.current.position.y = Math.sin(delta) * 100;
-  //   }
-  // });
-
-  return <sphere
-    ref={ref}
-    position={{ x: 50, y: -50, z: 0 }}
-    radius={50}
-    color={{ x: 0.5, y: 0.3, z: 0.6 }}
-    emissive={{ x: 0., y: 0., z: 0. }}
-    roughness={0}
-    specular={1}
-  />
-}
+import Page, { Camera } from '@youyouzone/react-sdf';
 
 export const App = () => {
+  const rawData = [
+    { position: { x: 0, y: 200, z: 0 }, size: { width: 100, height: 1, depth: 50 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 1, y: 1, z: 1 }, roughness: 0, specular: 0 },
+    { position: { x: 0, y: 201, z: 0 }, size: { width: 200, height: 1, depth: 100 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 0 },
+    { position: { x: -200, y: 0, z: 0 }, size: { width: 1, height: 200, depth: 100 }, color: { x: 1, y: 0., z: 0. }, emissive: { x: 0, y: 0, z: 0 }, roughness: 1, specular: 1 },
+    { position: { x: 200, y: 0, z: 0 }, size: { width: 1, height: 200, depth: 100 }, color: { x: 0., y: 1, z: 0. }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0.1, specular: 1 },
+    { position: { x: 0, y: 0, z: -100 }, size: { width: 200, height: 200, depth: 1 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 1 },
+    { position: { x: 0, y: -100, z: 0 }, size: { width: 200, height: 1, depth: 100 }, color: { x: 0.7, y: 0.7, z: 0.7 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 0 },
+    { position: { x: -50, y: -20, z: 50 }, size: { width: 30, height: 60, depth: 30 }, color: { x: 0.5, y: 0.3, z: 0.6 }, emissive: { x: 0, y: 0, z: 0 }, roughness: 0, specular: 0 },
+  ]
+
   return <Page
     style={{ width: '100%', height: '100vh' }}
   >
@@ -52,6 +31,14 @@ export const App = () => {
         specular={specular}
       />
     )}
-    <Sphere />
+    <sphere
+      ref={ref}
+      position={{ x: 50, y: -50, z: 0 }}
+      radius={50}
+      color={{ x: 0.5, y: 0.3, z: 0.6 }}
+      emissive={{ x: 0., y: 0., z: 0. }}
+      roughness={0}
+      specular={1}
+    />
   </Page>
 }
