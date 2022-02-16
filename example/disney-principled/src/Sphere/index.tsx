8b177becd792526fd783c780eadf86b737b8b468
Eric-Schecter <1090165925@qq.com>
2022-02-16 20:59:33 +0800

    remove warnings in example

--- example/disney-principled/src/Sphere/index.tsx
+++ example/disney-principled/src/Sphere/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useEffect, useState } from "react";
 import { GUIHelper } from "../gui";
 
@@ -12,24 +13,18 @@ export const Sphere = ({ gui }: { gui: GUIHelper | null }) => {
     clearcoatGloss: 0,
     specular: 0,
   });
-
+  const init = useCallback(() => gui?.init(state, setState), [gui, state]);
   useEffect(() => {
-    if (!gui) {
-      return;
-    }
-    gui.init(state, setState);
-  }, [gui])
+    init();
+  }, [init])
 
-  const { radius, color, roughness, metallic, specTrans, specColor, clearcoat, clearcoatGloss, specular } = state;
+  const { radius, roughness, metallic, specTrans, specular } = state;
   return <sphere
     radius={radius}
-    color={[1,1,1]}
+    color={[1, 1, 1]}
     roughness={roughness}
     metallic={metallic}
     specTrans={specTrans}
-    // specColor={specColor}
-    // clearcoat={clearcoat}
-    // clearcoatGloss={clearcoatGloss}
     specular={specular}
   />
 }
